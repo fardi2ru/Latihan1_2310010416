1fd213248b0cee9ea4bfb6b2fd9e487298bbd012
fardi2ru <farid.mankotabaru@gmail.com>
2025-06-09 20:27:16 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,5 +1,13 @@
 package hitung;
 
 public class MatematikaBeraksi {
+    public static void main(String[] args) {
+        Matematika farid = new Matematika(1,6);
+        
+        System.out.println("Hasil Penjumlahan:"+farid.setPenjumlahan());
+        System.out.println("Hasil Pengurangan:"+farid.setPengurangan());
+        System.out.println("Hasil Perkalian:"+farid.setPerkalian());
+        System.out.println("Hasil Pembagian:"+farid.setPembagian());
+    }
     
 }
